c8f4acf0b3b58d6845eb908a068a72816d8c2f54
AnkurKaushik <a123ankur@yahoo.com>
2021-02-08 14:24:25 -0600

    created a function to initialize an arraylist with 100 random numbers from 0-100

--- LongestIncreasing/src/main/java/com/example/longestincreasing/LongestIncreasingApplication.java
+++ LongestIncreasing/src/main/java/com/example/longestincreasing/LongestIncreasingApplication.java
@@ -1,10 +1,28 @@
 package com.example.longestincreasing;
 
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class LongestIncreasingApplication {
 
-	public static void main(String[] args) {
+	private static ArrayList<Integer> createRandArry()
+	{
+		ArrayList<Integer> list = new ArrayList<>();
+		for(int i=0; i < 100; i++)
+		{
+			list.add((int)(Math.random()*100));
+		}
+		return list;
+	}
 
+	private static int LongestSubsequence(ArrayList<Integer> inputArry)
+	{
+		return 0;	//placeholder
+	}
+
+	public static void main(String[] args) {
+		List l = createRandArry();
+		System.out.println(l);
 	}
 }
