7811ce2e5f25a9c83ad8c9557c8c73d264413171
AnkurKaushik <a123ankur@yahoo.com>
2021-02-08 14:29:01 -0600

    added few comments, updated createrandarry

--- LongestIncreasing/src/main/java/com/example/longestincreasing/LongestIncreasingApplication.java
+++ LongestIncreasing/src/main/java/com/example/longestincreasing/LongestIncreasingApplication.java
@@ -6,19 +6,27 @@ import java.util.List;
 
 public class LongestIncreasingApplication {
 
+	//inits new arraylist of length 100 with random ints between 0-100
 	private static ArrayList<Integer> createRandArry()
 	{
+		//output list
 		ArrayList<Integer> list = new ArrayList<>();
+		//adds the ints
 		for(int i=0; i < 100; i++)
-		{
 			list.add((int)(Math.random()*100));
-		}
+
 		return list;
 	}
 
+	//code to return the longest subsequence
 	private static int LongestSubsequence(ArrayList<Integer> inputArry)
 	{
-		return 0;	//placeholder
+		//holds value of current longest lengths starting at each index
+		ArrayList<Integer> temp = new ArrayList<>();
+		int max = 0;
+		
+
+		return max;
 	}
 
 	public static void main(String[] args) {
